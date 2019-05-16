7eaea7a2e68e71473f8405780da3bc27e08ce0ac
Ihor Radchenko <ihor.radchenko86@gmail.com>
2019-05-16 23:46:18 +0300

    added insert stmt

--- perl_plus_mysql_test.pl
+++ perl_plus_mysql_test.pl
@@ -9,6 +9,15 @@ my $host = "localhost";
 
 my $dbh = DBI->connect("DBI:mysql:test_schema:$host", $user, $pass);
 
+sub insert_new_row {
+    my ($id, $technology) = @_;
+
+    my $stmt = $dbh->prepare("insert into jobs(job_id, job_title) values($id, '$technology');");
+    $stmt->execute or die "error to perform sql query";
+}
+
+insert_new_row(9, "Oracle");
+
 my $stmt = $dbh->prepare("select * from jobs order by job_id desc");
 $stmt->execute
     or die "error to perform sql query";
@@ -16,5 +25,7 @@ $stmt->execute
 while (my @row = $stmt->fetchrow_array()) {
     print "$row[0] : $row[1]\n";
 }
+
+$dbh->disconnect();
 
 exit(0);
